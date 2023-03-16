453d08788842b4490bc2a0a4155ebaf9eff75788
Igor Mokhnatskyy <55652900+gigorko1993@users.noreply.github.com>
2023-03-16 14:02:54 +0200

    Revert "delete all changes"

---
+++ src/handlers/private.js
@@ -0,0 +1,18 @@
+const handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    headers: {
+        /* Required for CORS support to work */
+      'Access-Control-Allow-Origin': '*',
+        /* Required for cookies, authorization headers with HTTPS */
+      'Access-Control-Allow-Credentials': true,
+    },
+    body: JSON.stringify({
+      event,
+      context
+    }),
+  };
+}
+module.exports = {
+  handler
+}
